73b3919ccba74307f243bed566fd2b8766533273
LordGarfil <jotarestrepo01@gmail.com>
2021-11-28 22:52:30 -0500

    New feature | Se renderizan temporalmente las alertas

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="app.css">
   <title>Document</title>
 </head>
 <body>
   <?php 
-  phpinfo() 
+    require_once("./alert.php");
   ?>
 </body>
 </html>
