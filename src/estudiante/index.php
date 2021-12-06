d6ade3adb2f1497ca270912c5e61bf0e7ddb051d
Jean Carlos <jean.quejada@sioma.app>
2021-12-05 23:50:24 -0500

    NEW FEAT: diseño de la vista de estudiantes

--- src/estudiante/index.php
+++ src/estudiante/index.php
@@ -1,12 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="../styles/students.css">
+  <title>Llamados de atención</title>
 </head>
+
 <body>
-  estudiante
+  <!-- Cargando contenido -->
+  <!-- <div class="center_loader">
+    <div class="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div> -->
+
+  <!-- nav bar del contenido -->
+  <header>
+    <div class="header-content">
+      <div class="sena">
+        <h1>Sena</h1>
+      </div>
+      <div class="student-data">
+        <div class="ficha">
+          <h1>2056419</h1>
+        </div>
+        <b>|</b>
+        <div class="student-name">
+          <a href="">
+            Jean Carlos Quejada Toro
+          </a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Contenido de la tabla -->
+  <div class="body-content">
+    <div class="content">
+      <div class="route">
+        <h1>Estudiante > Llamados de atención</h1>
+      </div>
+      <div class="table">
+        <div class="item">
+          <div class="llamado">
+            <h1>Vocabulario ofensivo</h1>
+          </div>
+          <div class="articulo">
+            <h1>5006</h1>
+          </div>
+          <div class="docente">
+            <h1>
+              Juan Pablo Oquendo
+            </h1>
+          </div>
+          <div class="categoria">
+            leve
+          </div>
+        </div>
+        <div class="item">
+          <div class="llamado">
+            <h1>Vocabulario ofensivo</h1>
+          </div>
+          <div class="articulo">
+            <h1>5006</h1>
+          </div>
+          <div class="docente">
+            <h1>
+              Juan Pablo Oquendo
+            </h1>
+          </div>
+          <div class="categoria">
+            Grave
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
 </body>
+<Script:src="students.js">
+  </Script:src>
+  </Script>
+
 </html>
