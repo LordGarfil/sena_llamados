867105cd6974f3afcf8fa1079f802d2a239e1214
LordGarfil <jotarestrepo01@gmail.com>
2021-12-06 22:11:13 -0500

    Refactor | Se agregan campos necesarios para informacion de login

--- src/model/login.php
+++ src/model/login.php
@@ -10,8 +10,12 @@ function login($data = null)
       $user = $data['user'];
       $password = $data['password'];
 
-      $sql = "SELECT u.persona_id, u.rol_id, r.nombre rol
+      $sql = "SELECT u.persona_id, concat(p.nombre, ' ', p.apellido) nombre,
+       p.correo, u.rol_id, r.nombre rol, f.id ficha_id, f.nombre ficha
             from usuarios u
+            inner join personas p on u.persona_id = p.id
+            inner join personas_fichas pf on p.id = pf.persona_id
+            inner join fichas f on pf.ficha_id = f.id
             inner join roles r on u.rol_id = r.id
             where u.persona_id = ?
             and u.contrasena = ?";
