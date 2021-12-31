a4fa6ba7bb9936d8fc9922055b5ce7458575c3f4
LordGarfil <jotarestrepo01@gmail.com>
2021-12-31 11:07:11 -0500

    Get reglas

--- src/model/reglas.php
+++ src/model/reglas.php
@@ -1,5 +1,16 @@
 <?php
 
+function getReglas(){
+  require_once("db.php");
+  require_once("../functions.php");
+
+  $sql = "SELECT r.articulo, r.nombre, c.id categoria_id, c.nombre categoria, r.descripcion FROM reglas r
+    inner join categorias c on r.categoria_id = c.id";
+ 
+  $res = fetch($sql, []);
+  answer_json($res);
+}
+
 function createReglas($data = []){
   require_once("db.php");
   require_once("../functions.php");
@@ -12,4 +23,9 @@ function createReglas($data = []){
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
   // $data = $_POST ? $_POST : file_get_contents("php://input");
   createReglas($_POST);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+  // $data = $_POST ? $_POST : file_get_contents("php://input");
+  getReglas();
 }
